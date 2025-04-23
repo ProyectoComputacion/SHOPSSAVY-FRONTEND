2b8803ccaa654bb0d9ab237888a87e9410dbc16b
Juan Manuel Moreno <juanmanuelmorenoq@gmail.com>
2025-04-23 13:52:44 +0200

    Rediregimiento de Calificadas a Recetas

--- frontend/src/app/pages/clasificados/clasificados.component.ts
+++ frontend/src/app/pages/clasificados/clasificados.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RatingService } from '../../services/rating.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-comparacion',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],  
   templateUrl: './clasificados.component.html',
   styleUrls: ['./clasificados.component.scss']
 })
